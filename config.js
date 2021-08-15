d3d063739c26c0e45fb5dc457fdc74d9060541e3
Yuhaig159 <88963179+Yuhaig159@users.noreply.github.com>
2021-08-15 15:31:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     btnReply: 'Gửi cho ông xã',
     mess: 'HAPPY ANNIVERSARY 1 MONTHS ',
     messDesc: 'ANH YÊU EM. THƯƠNG BÀ XÃ.',
-    messDesc: 'Anh không biết nói gì hết với mới nên cũng hong mún nói nhiều. Anh chỉ mong mình mãi vui dà hạnh phúc nhe bà xã. 1 lần nữa là ANH YÊU EM',
+    messDesc: 'Anh không biết nói gì hết với mới nên cũng hong mún nói nhiều. Anh chỉ mong mình mãi vui dà hạnh phúc nhe bà xã. 1 lần nữa là ANH YÊU EM <3 ',
     btnAccept: 'EM CŨNG YÊU ANH, THƯƠNG ÔNG XÃAAAA',
  
 }
