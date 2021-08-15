7424285ab2d7799dcd060ecde2bbd898580a72bb
Yuhaig159 <88963179+Yuhaig159@users.noreply.github.com>
2021-08-15 15:24:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
 const CONFIG = {
     introTitle: 'Bà xãaaaaa à!',
-    btnIntro: 'hihi',
-    title: 'Bà xã có nhớ hôm nay là ngày gì hong taaaaa?',
+    btnIntro: 'Dạaaaaa',
+    title: 'Bà xã có nhớ hôm nay là ngày gì hong ta?',
     btnYes: 'Dạ nhớ á ông xã',
-    btnNo: 'Hong bít gì hếtttttt',
+    btnNo: 'Hong bít gì hếtt',
     question:'Vậy hôm nay là ngày gì á',
     btnReply: 'Gửi cho ông xã',
     mess: 'HAPPY ANNIVERSARY 1 MONTHS ',
-    messDesc: 'ANH YÊU EM. THƯƠNG BÀ XÃAAAA',
-    btnAccept: 'EM CŨNG YÊU ANH, THƯƠNG ÔNG ',
+    messDesc: 'ANH YÊU EM. THƯƠNG BÀ XÃ.',
+    messDesc: 'Anh không biết nói gì hết với mới nên cũng hong mún nói nhiều. Anh chỉ mong mình mãi vui dà hạnh phúc nhe bà xã. 1 lần nữa là ANH YÊU EM',
+    btnAccept: 'EM CŨNG YÊU ANH, THƯƠNG ÔNG XÃ',
  
 }
